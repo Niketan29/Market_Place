d0209fe0365e15ec9ce01fe9354647b6961eb49f
Niketan29 <147406585+Niketan29@users.noreply.github.com>
2026-02-20 17:31:28 +0530

    updates

--- web/src/api/client.js
+++ web/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://market-place-1-bo3z.onrender.com'
 })
 
 api.interceptors.request.use((config) => {
