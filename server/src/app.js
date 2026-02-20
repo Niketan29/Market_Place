1c3faaacd1abae0f6538309445eb2f5e0ce1463c
Niketan29 <147406585+Niketan29@users.noreply.github.com>
2026-02-20 17:21:51 +0530

    updates

--- server/src/app.js
+++ server/src/app.js
@@ -20,4 +20,8 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/auth", authRoutes);
 app.use("/products", productRoutes);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
